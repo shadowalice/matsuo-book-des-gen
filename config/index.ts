51ef753823a81ae7b39ab223e1cfd26abde1f480
shadowalice <shadowalice@hotmail.co.th>
2024-03-10 21:50:06 +0700

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,11 +3,11 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Text Generator APP',
-  description: 'App description',
-  copyright: '',
+  title: 'Matsuo AI Book Description Generator',
+  description: 'สร้างคำอธิบายหนังสือฟรีด้วย AI',
+  copyright: 'Matsuo IM',
   privacy_policy: '',
-  default_language: 'EN',
+  default_language: 'EN-us',
 }
 
 export const API_PREFIX = '/api'
